2462041b3ba224a0ed16d910f6a1421ad65c47e4
bian160928 <biankobayashi@gmail.com>
2020-08-27 21:32:11 +0900

    make_instrumental1

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,7 @@
 class TopsController < ApplicationController
   def index
   end
+
+  def beat
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'tops#index'
 
-  resources :tops
+  resources :tops do
+    collection do
+      get 'beat'
+    end
+  end
 end
